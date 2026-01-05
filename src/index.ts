9e9a442ad17ab577b4983b6a2929f817b8421db0
Eron Alves <eron.alves@rocketmail.com>
2026-01-05 00:51:33 -0300

    refactor: split into more files

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,9 @@
 import express from "express";
+import { router } from "./routes";
 
 const app = express();
 
-type Source = "SYNC" | "TIMEOUT" | "PROMISE_RESOLVE" | "NEXT_TICK";
-
-const SOURCE: Record<Source, Source> = {
-  NEXT_TICK: "NEXT_TICK",
-  TIMEOUT: "TIMEOUT",
-  PROMISE_RESOLVE: "PROMISE_RESOLVE",
-  SYNC: "SYNC",
-};
-
-function createLog(source: Source, log: string) {
-  return {
-    timestamp: Date.now(),
-    source,
-    log,
-  };
-}
-
-app.get("/level1", async (_, res) => {
-  console.log("Executing level 1");
-  const logAggregator: Array<ReturnType<typeof createLog>> = [];
-  logAggregator.push(createLog(SOURCE.SYNC, "executing sync code"));
-
-  const p1 = new Promise<void>((res) =>
-    setTimeout(() => {
-      logAggregator.push(
-        createLog(SOURCE.TIMEOUT, "executing from set timeout")
-      );
-      res();
-    }, 0)
-  );
-  const p2 = new Promise<void>((res) => {
-    res();
-  }).then(() => {
-    logAggregator.push(
-      createLog(SOURCE.PROMISE_RESOLVE, "executing from promise resolve")
-    );
-  });
-  const p3 = new Promise<void>((res) => {
-    process.nextTick(() => {
-      logAggregator.push(
-        createLog(SOURCE.NEXT_TICK, "executing from next tick")
-      );
-      res();
-    });
-  });
-
-  await Promise.all([p1, p2, p3]);
-
-  res.statusCode = 200;
-  res.send(logAggregator);
-});
+app.use(router);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
---
+++ src/level1.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from "express";
+
+type Source = "SYNC" | "TIMEOUT" | "PROMISE_RESOLVE" | "NEXT_TICK";
+
+const SOURCE: Record<Source, Source> = {
+  NEXT_TICK: "NEXT_TICK",
+  TIMEOUT: "TIMEOUT",
+  PROMISE_RESOLVE: "PROMISE_RESOLVE",
+  SYNC: "SYNC",
+};
+
+function createLog(source: Source, log: string) {
+  return {
+    timestamp: Date.now(),
+    source,
+    log,
+  };
+}
+
+export async function level1(_: Request, res: Response) {
+  console.log("Executing level 1");
+  const logAggregator: Array<ReturnType<typeof createLog>> = [];
+  logAggregator.push(createLog(SOURCE.SYNC, "executing sync code"));
+
+  const p1 = new Promise<void>((res) =>
+    setTimeout(() => {
+      logAggregator.push(
+        createLog(SOURCE.TIMEOUT, "executing from set timeout")
+      );
+      res();
+    }, 0)
+  );
+  const p2 = new Promise<void>((res) => {
+    res();
+  }).then(() => {
+    logAggregator.push(
+      createLog(SOURCE.PROMISE_RESOLVE, "executing from promise resolve")
+    );
+  });
+  const p3 = new Promise<void>((res) => {
+    process.nextTick(() => {
+      logAggregator.push(
+        createLog(SOURCE.NEXT_TICK, "executing from next tick")
+      );
+      res();
+    });
+  });
+
+  await Promise.all([p1, p2, p3]);
+
+  res.statusCode = 200;
+  res.send(logAggregator);
+}
