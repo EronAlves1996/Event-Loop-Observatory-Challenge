62f9c847b47f6e81c354a04cf8d0bd77a677c607
Eron Alves <eron.alves@rocketmail.com>
2026-01-07 00:11:40 -0300

    feat: explain why timeout is logged first

--- src/level2/index.ts
+++ src/level2/index.ts
@@ -5,31 +5,40 @@ type Phase = "IMMEDIATE" | "TIMEOUT" | "READ_FILE";
 
 const expectedPhases: Array<Phase> = ["TIMEOUT", "READ_FILE", "IMMEDIATE"];
 
+// The function is handled as part of poll phase, because, the callback
+// is invoked as a result of 'connection arrived' event
 export async function level2(_: Request, res: Response) {
   console.log("Executing level 2");
   const phases: Array<Phase> = [];
 
-  setImmediate(() => {
-    console.log("set immediate file read");
-    phases.push("IMMEDIATE");
+  // When we setImmediate here, we force the event loop
+  // to go right to the next cycle. This callback here
+  // will be executed on the 'check' phase. The next
+  // event loop cycle will start on 'timers' phase, making
+  // the timeout be logged first
+  setImmediate(async () => {
+    setImmediate(() => {
+      console.log("set immediate file read");
+      phases.push("IMMEDIATE");
+    });
+
+    setTimeout(() => {
+      console.log("set timeout on file read");
+      phases.push("TIMEOUT");
+    });
+
+    const fullfill = readFile("./README.md").then(() => {
+      console.log("file readed");
+      phases.push("READ_FILE");
+    });
+
+    await fullfill;
+
+    res.statusCode = 200;
+    res.send(
+      JSON.stringify(phases) === JSON.stringify(expectedPhases)
+        ? "File read completed in the expected phase"
+        : "File completed in unexpected phase"
+    );
   });
-
-  setTimeout(() => {
-    console.log("set timeout on file read");
-    phases.push("TIMEOUT");
-  });
-
-  const fullfill = readFile("./README.md").then(() => {
-    console.log("file readed");
-    phases.push("READ_FILE");
-  });
-
-  await fullfill;
-
-  res.statusCode = 200;
-  res.send(
-    JSON.stringify(phases) === JSON.stringify(expectedPhases)
-      ? "File read completed in the expected phase"
-      : "File completed in unexpected phase"
-  );
 }
