970a579862ada54325ebd73dcd1941619bcf196a
Eron Alves <eron.alves@rocketmail.com>
2026-01-07 01:16:58 -0300

    feat: put measurement marks

--- src/level2/index.ts
+++ src/level2/index.ts
@@ -1,9 +1,7 @@
 import { Request, Response } from "express";
-import { readFile } from "node:fs/promises";
+import { readFile } from "node:fs";
 
 type Phase = "IMMEDIATE" | "TIMEOUT" | "READ_FILE";
-
-const expectedPhases: Array<Phase> = ["TIMEOUT", "READ_FILE", "IMMEDIATE"];
 
 // The function is handled as part of poll phase, because, the callback
 // is invoked as a result of 'connection arrived' event
@@ -16,29 +14,47 @@ export async function level2(_: Request, res: Response) {
   // will be executed on the 'check' phase. The next
   // event loop cycle will start on 'timers' phase, making
   // the timeout be logged first
-  setImmediate(async () => {
+  const { promise: filePoromise, resolve: resolveFile } =
+    Promise.withResolvers<void>();
+  const { promise: immediatePromise, resolve: resolveImmediate } =
+    Promise.withResolvers<void>();
+
+  const { promise: timeoutProm, resolve: resolveTimeout } =
+    Promise.withResolvers<void>();
+
+  performance.clearMeasures();
+  const p1 = performance.mark("before_file_reading");
+
+  // In some cases, the file reading can happen under 1 ms. The measurements
+  // marks was put to view this perspective
+  setTimeout(() => {
+    console.log("set timeout on file read");
+    phases.push("TIMEOUT");
+    resolveTimeout();
+  }, 0);
+
+  readFile("./package.json", (err, data) => {
+    console.log("file readed");
+    performance.mark("after_file_reading");
+    console.log(
+      "Elapsed time: " +
+        performance.measure(
+          "duration",
+          "before_file_reading",
+          "after_file_reading"
+        ).duration
+    );
+    phases.push("READ_FILE");
+    resolveFile();
+
     setImmediate(() => {
       console.log("set immediate file read");
       phases.push("IMMEDIATE");
+      resolveImmediate();
     });
-
-    setTimeout(() => {
-      console.log("set timeout on file read");
-      phases.push("TIMEOUT");
-    });
-
-    const fullfill = readFile("./README.md").then(() => {
-      console.log("file readed");
-      phases.push("READ_FILE");
-    });
-
-    await fullfill;
-
-    res.statusCode = 200;
-    res.send(
-      JSON.stringify(phases) === JSON.stringify(expectedPhases)
-        ? "File read completed in the expected phase"
-        : "File completed in unexpected phase"
-    );
   });
+
+  await Promise.all([immediatePromise, timeoutProm, filePoromise]);
+  res.statusCode = 200;
+  res.send(phases);
 }
