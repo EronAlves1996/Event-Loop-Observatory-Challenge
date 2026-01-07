808bb93aef048b6f37fd9a3ddfc37bf93fdf444a
Eron Alves <eron.alves@rocketmail.com>
2026-01-06 23:54:23 -0300

    feat: create structure

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { level1 } from "./level1";
+import { level2 } from "./level2";
 
 export const router = Router();
 
 router.get("/level1", level1);
+router.get("/level2", level2);
